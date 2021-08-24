992e5e32fc2b7350da65fb21aa316376c88e5366
galifornia <martinho87@gmail.com>
2021-08-24 10:30:39 +0200

    0344 Reverse String

---
+++ 0344_reverse_string.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func reverseString(s []byte) {
+	size := len(s)
+	left := 0
+	right := size - 1
+
+	for left < right {
+		aux := s[right]
+		s[right] = s[left]
+		s[left] = aux
+		left++
+		right--
+	}
+
+}
+
+func main() {
+	s := []byte{'h', 'e', 'l', 'l', 'o'}
+	// ["o","l","l","e","h"]
+	fmt.Println(s)
+	reverseString(s)
+}
