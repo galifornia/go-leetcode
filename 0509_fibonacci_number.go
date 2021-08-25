b84fa3fc8876ddf07b820770b18e25b51e7d8261
galifornia <martinho87@gmail.com>
2021-08-25 17:10:06 +0200

    0509 Fibonacci number without map

--- 0509_fibonacci_number.go
+++ 0509_fibonacci_number.go
@@ -18,18 +18,45 @@ func fib(n int) int {
 		return 1
 	}
 
-	// initialize first 2 values in map
-	memory := make(map[int]int)
-	memory[0] = 1
-	memory[1] = 1
+	// initialize two pair of values from n = 3
+	prev := 1
+	last := 2
 
-	// build each new value from the previous two numbers already calculated in the memory map
+	// update last with the sum of the two and remember the last one
 	for i := 2; i < n; i++ {
-		memory[i] = memory[i-1] + memory[i-2]
+		aux := prev
+		prev = last
+		last = aux + prev
 	}
 
-	return memory[n-1]
+	return last
 }
+
+// version using a map to store calculations
+
+// func fib(n int) int {
+// 	if n < 0 {
+// 		return -1
+// 	}
+
+//     if n == 0 {
+// 		return 0
+// 	}
+
+// 	if n < 3 {
+// 		return 1
+// 	}
+
+// 	memory := make(map[int]int)
+// 	memory[0] = 1
+// 	memory[1] = 1
+
+// 	for i := 2; i < n; i++ {
+// 		memory[i] = memory[i-1] + memory[i-2]
+// 	}
+
+// 	return memory[n-1]
+// }
 
 func main() {
 	fmt.Println(fib(5))
