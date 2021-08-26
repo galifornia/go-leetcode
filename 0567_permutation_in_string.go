2c16424858e777aea14e14e096be6da47bad8a84
galifornia <martinho87@gmail.com>
2021-08-26 18:00:53 +0200

    0567: add performance gains O(n)

--- 0567_permutation_in_string.go
+++ 0567_permutation_in_string.go
@@ -4,32 +4,46 @@ func checkInclusion(s1 string, s2 string) bool {
 	// Return if s1 is smaller
 	if len(s1) > len(s2) {
 		return false
+
 	}
 
-	// map that contains number of times of each character in s1
-	runeMap := make(map[rune]int)
-	for _, c := range s1 {
-		runeMap[c]++
+	size1 := len(s1)
+	size2 := len(s2)
+
+	// create maps for s1 (permutation string) and s2
+	countMap1 := make(map[byte]int)
+	countMap2 := make(map[byte]int)
+
+	// initialize map counting characters in both s1 & first s2 window
+	for i := 0; i < len(s1); i++ {
+		countMap1[s1[i]]++
+		countMap2[s2[i]]++
 	}
 
-	// loop through s2 using a sliding window of size len(s1)
-	for i := 0; i < len(s2)-len(s1)+1; i++ {
-		// create count map for this sliding window
-		workingMap := make(map[rune]int)
-		for j := i; j < i+len(s1); j++ {
-			workingMap[rune(s2[j])]++
-		}
-		// Check if there is match between maps
-		ok := matches(&runeMap, &workingMap)
-		if ok {
+	// check if we are lucky and our first window matches
+	if matches(&countMap1, &countMap2) {
+		return true
+	}
+
+	i, j := 1, size1
+	// loop until window is at the end of string s2
+	for j < size2 {
+		// we move the window 1 so we remove the character that gets out of the window
+		countMap2[s2[i-1]]--
+		countMap2[s2[j]]++
+
+		if matches(&countMap1, &countMap2) {
 			return true
 		}
+
+		i++
+		j++
 	}
 
 	return false
 }
 
-func matches(m1, m2 *map[rune]int) bool {
+func matches(m1, m2 *map[byte]int) bool {
 	for k, v := range *m1 {
 		if v != (*m2)[k] {
 			return false
@@ -43,7 +57,7 @@ func matches(m1, m2 *map[rune]int) bool {
 // 	// s2 := "ooolleoooleh"
 // 	// s1 := "adc"
 // 	// s2 := "dcda"
-// 	s1 := "eidboaoo"
-// 	s2 := "ab"
+// 	s2 := "eidbaooo"
+// 	s1 := "ab"
 // 	fmt.Println(checkInclusion(s1, s2))
 // }
