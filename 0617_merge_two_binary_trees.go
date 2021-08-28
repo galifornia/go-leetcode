79b1ff257fc847edf95b3dff63d11be88b6420c8
galifornia <martinho87@gmail.com>
2021-08-28 13:10:34 +0200

    0617 Merge Two Binary Trees improved version

--- 0617_merge_two_binary_trees.go
+++ 0617_merge_two_binary_trees.go
@@ -7,17 +7,16 @@ type TreeNode struct {
 }
 
 func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
-	if root1 == nil && root2 == nil {
-		return nil
-	} else if root1 == nil {
-		return &TreeNode{Val: root2.Val, Left: mergeTrees(nil, root2.Left), Right: mergeTrees(nil, root2.Right)}
-	} else if root2 == nil {
-		return &TreeNode{Val: root1.Val, Left: mergeTrees(root1.Left, nil), Right: mergeTrees(root1.Right, nil)}
-	} else {
-		return &TreeNode{Val: root2.Val, Left: mergeTrees(root1.Left, root2.Left), Right: mergeTrees(root1.Right, root2.Right)}
+	if root1 == nil {
+		return root2
 	}
-}
 
-func main() {
+	if root2 == nil {
+		return root1
+	}
 
+	root := &TreeNode{Val: root1.Val + root2.Val}
+	root.Left = mergeTrees(root1.Left, root2.Left)
+	root.Right = mergeTrees(root1.Right, root2.Right)
+	return root
 }
