6bc1b49f513fe8e2cb79df64742486c7451e617a
galifornia <martinho87@gmail.com>
2021-08-31 21:45:52 +0200

    0046 Permutations

---
+++ 0046_permutations.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func permute(nums []int) [][]int {
+	var res [][]int
+	permuteRec([]int{}, nums, &res)
+	return res
+}
+
+// We use a pointer for the result so we don't need to worry returning it.
+func permuteRec(currComb, left []int, res *[][]int) {
+	// We know that we found a new combination when we have no elements left.
+	if len(left) == 0 {
+		*res = append(*res, currComb)
+		return
+	}
+	// For the next iteration we consider all the left elements but the current one (idx).
+	for idx, l := range left {
+		permuteRec(
+			append(currComb, l),
+			append(append([]int{}, left[:idx]...), left[idx+1:]...), // Make sure to allocate a new slice.
+			res,
+		)
+	}
+}
+
+func main() {
+	nums := []int{1, 2, 3}
+	fmt.Println(permute(nums))
+}
