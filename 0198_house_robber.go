e07a14686cece87007567fe18754414e6655840a
galifornia <martinho87@gmail.com>
2021-09-01 23:42:13 +0200

    0198 House Robber recursion with memoization

--- 0198_house_robber.go
+++ 0198_house_robber.go
@@ -1,24 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"math"
 )
 
 func rob(nums []int) int {
-	return findBiggestReward(nums, len(nums)-1)
+	size := len(nums)
+	memo := make([]int, size+1)
+
+	for i := 0; i < size+1; i++ {
+		memo[i] = math.MinInt32
+	}
+	return findBiggestReward(nums, size-1, &memo)
 }
 
-func findBiggestReward(nums []int, i int) int {
+func findBiggestReward(nums []int, i int, memo *[]int) int {
 	if i < 0 {
 		return 0
 	}
-	return int(math.Max(float64(findBiggestReward(nums, i-2)+nums[i]), float64(findBiggestReward(nums, i-1))))
+	if (*memo)[i] >= 0 {
+		return (*memo)[i]
+	}
+	result := int(math.Max(float64(findBiggestReward(nums, i-2, memo)+nums[i]), float64(findBiggestReward(nums, i-1, memo))))
+	(*memo)[i] = result
+
+	return result
 }
 
-func main() {
-	// nums := []int{1, 2, 3, 1}
-	// nums := []int{2, 7, 9, 3, 1}
-	nums := []int{1, 4, 1, 2, 3, 1, 1, 5, 1}
-	fmt.Println(rob(nums))
-}
+// func main() {
+// 	// nums := []int{1, 2, 3, 1}
+// 	// nums := []int{2, 7, 9, 3, 1}
+// 	nums := []int{1, 4, 1, 2, 3, 1, 1, 5, 1}
+// 	fmt.Println(rob(nums))
+// }
