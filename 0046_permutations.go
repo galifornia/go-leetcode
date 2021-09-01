4f3d060cd33997af4a3aa35b18518a72593996a9
galifornia <martinho87@gmail.com>
2021-09-01 22:30:37 +0200

    0784 Permutations

--- 0046_permutations.go
+++ 0046_permutations.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func permute(nums []int) [][]int {
 	var res [][]int
 	permuteRec([]int{}, nums, &res)
@@ -27,7 +23,7 @@ func permuteRec(currComb, left []int, res *[][]int) {
 	}
 }
 
-func main() {
-	nums := []int{1, 2, 3}
-	fmt.Println(permute(nums))
-}
+// func main() {
+// 	nums := []int{1, 2, 3}
+// 	fmt.Println(permute(nums))
+// }
---
+++ 0784_letter_case_permutation.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func letterCasePermutation(s string) []string {
+	output := make([]string, 0)
+	for i := 0; i < len(s); i++ {
+		if !CheckIfNumber(s[i]) {
+			permString := make([]byte, len(s))
+			for idx := range permString {
+				permString[idx] = s[idx]
+			}
+
+			if strings.ToLower(string(s[i]))[0] == s[i] {
+				permString[i] = strings.ToUpper(string(s[i]))[0]
+			} else {
+				permString[i] = strings.ToLower(string(s[i]))[0]
+			}
+
+			output = append(output, string(permString))
+		}
+	}
+	return output
+}
+
+func CheckIfNumber(r byte) bool {
+
+	if r >= 48 && r <= 57 {
+		return false
+	}
+
+	return true
+}
+
+func main() {
+	s := "a1b2"
+	fmt.Println(letterCasePermutation(s))
+}
