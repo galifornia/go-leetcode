8f07a351c5fbf5c70f55a59da66c6a380494a61a
galifornia <martinho87@gmail.com>
2021-09-02 18:09:25 +0200

    0231 The Power of Two (bitwise)

--- 0231_the_power_of_two.go
+++ 0231_the_power_of_two.go
@@ -1,19 +1,5 @@
 package main
 
 func isPowerOfTwo(n int) bool {
-	if n == 1 {
-		return true
-	}
-
-	sum := 1
-	i := 1
-	for {
-		sum *= 2
-		if sum == n {
-			return true
-		} else if sum > n {
-			return false
-		}
-		i++
-	}
+	return n > 0 && n&(n-1) == 0
 }
