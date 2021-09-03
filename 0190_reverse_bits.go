7218ed462d8579d0d85e694717389aece46e1f81
galifornia <martinho87@gmail.com>
2021-09-03 19:06:49 +0200

    0190 Reverse Bits

---
+++ 0190_reverse_bits.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func reverseBits(num uint32) uint32 {
+	if num == 0 {
+		return 0
+	}
+
+	var result uint32 = 0
+
+	for i := 0; i < 32; i++ {
+		result <<= 1
+		if num&1 == 1 {
+			result++
+		}
+		num >>= 1
+	}
+	return result
+}
+
+func main() {
+	n := uint32(43261596)
+
+	fmt.Println(reverseBits(n))
+
+}
